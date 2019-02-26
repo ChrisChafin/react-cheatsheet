aa5b857e9d799a2867c6cf5de437630ef9072cdd
Chris Chafin <chris182@verizon.net>
2019-02-26 16:24:29 -0600

    Adding more files

---
+++ basic-components.js
@@ -0,0 +1,69 @@
+//simplest way to define a component
+function Welcome(props){
+  return <h1>Hello, {props.name}</h1>;
+}
+
+//Defining using ES6 class
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+//Rendering wiht user-defined components
+const element = <Welcome name="Chris" />;
+//this renders with props.name set to "Chris"
+
+//Spliting components into smaller ones
+function formatDate(date){
+  return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img
+      className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo(props){
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props){
+  return(
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+
+ReactDOM.render(
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
+  document.getElementById('root')
+);
--- children.js
+++ children.js
@@ -3,15 +3,16 @@
 */
 
 // It’s not guaranteed that props.children will be an array
-
-<Parent>
+const noChidren = (
+  <Parent>
   <h1>Welcome!</h1>
 </Parent>
-
+)
 // Right now, props.children is an Object, not an array
 // React only makes props.children an array if there are more than one child elements, like this
-
-<Parent>
-  <h1>Welcome</h1>
-  <h2>props.children will now be an array</h2>
-</Parent>
+const hasChildren = (
+  <Parent>
+    <h1>Welcome</h1>
+    <h2>props.children will now be an array</h2>
+  </Parent>
+)
